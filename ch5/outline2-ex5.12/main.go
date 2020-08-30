321a74bb2c14ad0ecc1cba94c40a674c730763ba
alextuan1024 <alextuan1024@gmail.com>
2020-08-30 16:36:22 +0800

    ex5.12

---
+++ ch5/outline2-ex5.12/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("outline: usage: outline url\n")
+	}
+	if err := outline(os.Args[1]); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func outline(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var depth int
+	var startElement = func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+			depth++
+		}
+	}
+	var endElement = func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			depth--
+			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+		}
+	}
+	forEachNode(doc, startElement, endElement)
+	return nil
+
+}
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}

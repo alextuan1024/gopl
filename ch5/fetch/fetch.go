5b29837e443ccd21f2eda91cd3c1ce848ff0ca89
alextuan1024 <alextuan1024@gmail.com>
2020-09-03 17:40:02 +0800

    cli fetch

--- ch5/fetch/fetch.go
+++ ch5/fetch/fetch.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path"
 )
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("usage: fetch url")
+	}
+	url := os.Args[1]
+	filename, size, err := fetch(url)
+	if err != nil {
+		log.Printf("fetch from url: %s error: %v\n", url, err)
+		os.Exit(1)
+	}
+	log.Printf("saved url: %s as file: %s, size: %v", url, filename, size)
+}
 
 func fetch(url string) (filename string, n int64, err error) {
 	resp, err := http.Get(url)
@@ -14,7 +28,7 @@ func fetch(url string) (filename string, n int64, err error) {
 	}
 	defer resp.Body.Close()
 	local := path.Base(resp.Request.URL.Path)
-	if local == "/" {
+	if local == "/" || local == "." {
 		local = "index.html"
 	}
 	f, err := os.Create(local)
